d9c427fc8f6ebe5fbac5846d761a9faec45e3fad
Bruno Pires <brunopires.ctto@gmail.com>
2023-08-01 13:06:43 -0300

    Exericio - Set 2.2

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,37 +4,42 @@ import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
-import entities.User;
-
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		Set<User> user = new HashSet<>();
+		
+		Set<Integer> a = new HashSet<>();
+		Set<Integer> b = new HashSet<>();
+		Set<Integer> c = new HashSet<>();
 		
 		System.out.print("How many students for a course A? ");
-		int a = sc.nextInt();
-		for (int i=0; i<a; i++) {
+		int n = sc.nextInt();
+		for (int i=0; i<n; i++) {
 			int code = sc.nextInt();
-			user.add(new User(code));
+			a.add(code);
 		}
 		
 		System.out.print("How many students for a course B? ");
-		int b = sc.nextInt();
-		for (int i=0; i<b; i++) {
+		n = sc.nextInt();
+		for (int i=0; i<n; i++) {
 			int code = sc.nextInt();
-			user.add(new User(code));
+			c.add(code);
 		}
 		
 		System.out.print("How many students for a course C? ");
-		int c = sc.nextInt();
-		for (int i=0; i<c; i++) {
+		n = sc.nextInt();
+		for (int i=0; i<n; i++) {
 			int code = sc.nextInt();
-			user.add(new User(code));
+			c.add(code);
 		}
 		
-		System.out.println("Total students: " + user.size());
+		Set<Integer> total = new HashSet<>(a);
+		total.addAll(b);
+		total.addAll(c);
+		
+		System.out.println("Total students: " + total.size());
 							
 		sc.close();
 	}
